1884521c111e17d06fb78ae7161bf832a7b13dd2
gibtang <gibtang@gmail.com>
2014-09-24 19:34:57 +0800

    Update cpp-screenshot.cpp

--- cpp-screenshot/cpp-screenshot/cpp-screenshot.cpp
+++ cpp-screenshot/cpp-screenshot/cpp-screenshot.cpp
@@ -16,25 +16,34 @@ void gdiscreen()
 	GdiplusStartupInput gdiplusStartupInput;
 	ULONG_PTR gdiplusToken;
 	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-
 	{
 		HDC scrdc, memdc;
 		HBITMAP membit;
-		scrdc = ::GetDC(0);
+		HWND hwnd = GetActiveWindow();
+		scrdc = ::GetDC(hwnd);
+		//scrdc = ::GetDC(0);//Get device context to start getting instance of screen to do grabbing//Check address of scrdc is not 0
+		assert( scrdc != NULL ); 
 		int Height = GetSystemMetrics(SM_CYSCREEN);
+		assert( Height != 0 ); 
 		int Width = GetSystemMetrics(SM_CXSCREEN);
+		assert( Width != 0 ); 
 		memdc = CreateCompatibleDC(scrdc);
+		assert( memdc != NULL ); 
 		membit = CreateCompatibleBitmap(scrdc, Width, Height);
+		assert( membit != NULL ); 
 		HBITMAP hOldBitmap = (HBITMAP)SelectObject(memdc, membit);
+		assert( hOldBitmap != NULL ); 
+		//transfer the screendata from position 0,0 to witdth/height from screen device context(scrdc) to memory device context (memdc)
 		BitBlt(memdc, 0, 0, Width, Height, scrdc, 0, 0, SRCCOPY);
-		Gdiplus::Bitmap bitmap(membit, NULL);
-		CLSID clsid;
+		Gdiplus::Bitmap bitmap(membit, NULL);//create bitmap and initialized it witl null values
+		assert( bitmap != NULL ); 
+		CLSID clsid;//A CLSID is a globally unique identifier that identifies a COM class object
 		GetEncoderClsid(L"image/jpeg", &clsid);
-		bitmap.Save(L"C:\\screen.jpeg", &clsid);
+		bitmap.Save(L"C:\\temp2\\screen.jpeg", &clsid);
 		SelectObject(memdc, hOldBitmap);
-		DeleteObject(memdc);
-		DeleteObject(membit);
-		::ReleaseDC(0, scrdc);
+		DeleteObject(memdc);//clean up to avoid memory leak
+		DeleteObject(membit);//clean up to avoid memory leak
+		::ReleaseDC(0, scrdc);//release the device context else it will show weird stuff
 	}
 	GdiplusShutdown(gdiplusToken);
 }
@@ -64,6 +73,7 @@ int GetEncoderClsid(const WCHAR* format, CLSID* pClsid)
 	free(pImageCodecInfo);
 	return 0;
 }
+
 int main()
 {
 	gdiscreen();
